e7c8d0d76d268786a34923be60c63686dd4eb7b7
manu2g <galdames.m.02@gmail.com>
2025-11-27 16:47:27 -0300

    pequeño fix

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import MedicPage from './pages/MedicPage'
 import AppointmentPage from './pages/appointmentPage'
 import PatientPage from './pages/patientPage'
+import MedicFormularioPage from './pages/medicFormularioPage'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/medicPage" element={<MedicPage />} />
         <Route path="/appointmentPage" element={<AppointmentPage />} />
         <Route path="/patientPage" element={<PatientPage />} />
+        <Route path="/medics/new" element={<MedicFormularioPage />} />
         <Route path="*" element={<h2>404 — Página no encontrada</h2>} />
       </Routes>
     </>
